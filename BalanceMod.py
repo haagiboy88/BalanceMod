885d29a94f26e71d6a8b666b52b40fd31926d0e9
jnesta <j.nesta@gmail.com>
2015-09-22 16:08:00 -0400

    code cleanup

--- BalanceMod.py
+++ BalanceMod.py
@@ -23,7 +23,9 @@ trinket_icons_path = "otherFiles/trinkets/"
 builds = ET.parse('otherFiles/builds.xml').getroot()
 
 # Subroutines
-def regkey_value(path, name="", start_key = None): # Used to find the path to Steam
+
+# regkey_value - Used to find the path to Steam
+def regkey_value(path, name="", start_key = None):
 	if isinstance(path, str):
 		path = path.split("\\")
 	if start_key is None:
@@ -48,9 +50,11 @@ def newRandomSeed():
 	seedIsRNG = ''.join(choice(ascii_uppercase + digits) for _ in range(6))
 	entryseed.set(seedIsRNG)
 
-# **** Practice Window Stuff ****
+# ---------------------
+# Practice Window Stuff
+# ---------------------
 
-# Load the image from the provided path in a format suitable for Tk
+# get_image - Load the image from the provided path in a format suitable for Tk
 def get_image(path):
 	from os import sep
 	image = _image_library.get(path)
@@ -60,7 +64,7 @@ def get_image(path):
 		_image_library[path] = image
 	return image
 
-# Return an item dictionary based on the provided item id or name
+# get_item_dict - Return an item dictionary based on the provided item id or name
 def get_item_dict(id):
 	id = str(id)
 	if id.isdigit():
@@ -72,7 +76,7 @@ def get_item_dict(id):
 			if child.attrib['name'] == id and child.tag != 'trinket':
 				return child.attrib
 
-# Given an item name or id, return its id
+# get_item_id - Given an item name or id, return its id
 def get_item_id(id):
 	id = str(id)
 	if id.isdigit():
@@ -84,7 +88,7 @@ def get_item_id(id):
 			if child.attrib['name'] == id and child.tag != 'trinket':
 				return child.attrib['id']
 
-# Given an item name or id, return its id
+# get_trinket_id - Given an item name or id, return its id
 def get_trinket_id(id):
 	id = str(id)
 	if id.isdigit():
@@ -96,7 +100,7 @@ def get_trinket_id(id):
 			if child.attrib['name'] == id and child.tag == 'trinket':
 				return child.attrib['id']
 
-# Given the name or id of an item, return its icon image
+# get_item_icon - Given the name or id of an item, return its icon image
 def get_item_icon(id):
 	dict = get_item_dict(id)
 	if dict:
@@ -105,7 +109,7 @@ def get_item_icon(id):
 	else:
 		return get_image(items_icons_path + "questionmark.png")
 
-# Given the name or id of a trinket, return its icon image
+# get_trinket_icon - Given the name or id of a trinket, return its icon image
 def get_trinket_icon(id):
 	id = str(id)
 	if id.isdigit():
@@ -118,7 +122,7 @@ def get_trinket_icon(id):
 				return get_image(trinket_icons_path + child.attrib['gfx'])
 	return get_image(trinket_icons_path + "questionmark.png")
 
-# Process the hearts image into the individual heart icons and return them
+# get_heart_icons - Process the hearts image into the individual heart icons and return them
 def get_heart_icons():
 	hearts_list = [None]*10
 	hearts = Image.open('otherFiles/ui_hearts.png')
@@ -134,7 +138,7 @@ def get_heart_icons():
 	hearts_list[8] = ImageTk.PhotoImage(hearts.crop((48,16,64,32)))# half black
 	return hearts_list
 
-# Display the practice mode window
+# practiceWindow - Display the practice mode window
 def practiceWindow(root):
 	global pWin
 	if not pWin:
@@ -204,48 +208,55 @@ def practiceWindow(root):
 		canvas.configure(scrollregion=canvas.bbox("all"), width=200, height=200)
 		canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
 
-		# Scrolling code taken from:
-		# http://stackoverflow.com/questions/16188420/python-tkinter-scrollbar-for-frame
+		# Scrolling code taken from: http://stackoverflow.com/questions/16188420/python-tkinter-scrollbar-for-frame
 		imageBox = LabelFrame(canvas, borderwidth=0)
 		interior_id = canvas.create_window(0, 0, window=imageBox, anchor=NW)
 
-		# track changes to the canvas and frame width and sync them, also updating the scrollbar
+		# _configure_interior - Track changes to the canvas and frame width and sync them (while also updating the scrollbar)
 		def _configure_interior(event):
-			# update the scrollbars to match the size of the inner frame
+			# Update the scrollbars to match the size of the inner frame
 			size = (imageBox.winfo_reqwidth(), imageBox.winfo_reqheight())
 			canvas.config(scrollregion="0 0 %s %s" % size)
 			if imageBox.winfo_reqwidth() != canvas.winfo_width():
-				# update the canvas's width to fit the inner frame
+				# Update the canvas's width to fit the inner frame
 				canvas.config(width=imageBox.winfo_reqwidth())
 		imageBox.bind('<Configure>', _configure_interior)
+
 		def _configure_canvas(event):
 			if imageBox.winfo_reqwidth() != imageBox.winfo_width():
-				# update the inner frame's width to fill the canvas
+				# Update the inner frame's width to fill the canvas
 				canvas.itemconfigure(interior_id, width=canvas.winfo_width())
 		canvas.bind('<Configure>', _configure_canvas)
-		# http://stackoverflow.com/questions/17355902/python-tkinter-binding-mousewheel-to-scrollbar
+
+		# _on_mousewheel - Code taken from: http://stackoverflow.com/questions/17355902/python-tkinter-binding-mousewheel-to-scrollbar
 		def _on_mousewheel(event):
-			canvas.yview_scroll(-1*(event.delta/120), "units")
-		# Yes, this works anywhere in the program. Will need to change if it ever has more than one scrollbar.
+			canvas.yview_scroll(-1 * (event.delta / 120), "units") # This works anywhere in the program; will need to change if there is ever more than one scrollbar
+
+		# Define keyboard bindings
 		root.bind_all("<MouseWheel>", _on_mousewheel)
 		pWin.bind("<Home>", lambda event: canvas.yview_moveto(0))
 		pWin.bind("<End>", lambda event: canvas.yview_moveto(1))
-		pWin.bind("<Prior>", lambda event: canvas.yview_scroll(-1,'pages'))
-		pWin.bind("<Next>", lambda event: canvas.yview_scroll(1,'pages'))
+		pWin.bind("<Prior>", lambda event: canvas.yview_scroll(-1, 'pages'))
+		pWin.bind("<Next>", lambda event: canvas.yview_scroll(1, 'pages'))
 
+		# Start to build the GUI window
 		hearts_list = get_heart_icons()
 		Label(imageBox, text="Click a build to play it", font="font 32 bold").pack(pady=5)
 		current_bgcolor = '#949494'
 		for child in builds:
+			# Background color
 			current_bgcolor = '#E0E0E0' if current_bgcolor == '#949494' else '#949494'
+
 			# Draw the build frame here
 			build_frame = LabelFrame(imageBox, bg=current_bgcolor)
 			build_frame.bind("<Button-1>", select_build)
 			build_frame.build = child
+
 			# ID
 			widget = Label(build_frame, text=child.attrib['id'], font="font 32 bold", bg=current_bgcolor, width=2)
 			widget.bind("<Button-1>", select_build)
 			widget.grid(row=0, rowspan=3)
+
 			# Items
 			widget = Label(build_frame, text="Items: ", bg=current_bgcolor)
 			widget.bind("<Button-1>", select_build)
@@ -268,6 +279,7 @@ def practiceWindow(root):
 				widget.bind("<Button-1>", select_build)
 				widget.grid(row=0, column=len(items)+1)
 			items_frame.grid(row=0, column=2, sticky=W)
+
 			# Health
 			widget = Label(build_frame, text="Health: ", bg=current_bgcolor)
 			widget.bind("<Button-1>", select_build)
@@ -279,6 +291,7 @@ def practiceWindow(root):
 			hearts_frame = make_hearts_frame(build_frame, redhearts, soulhearts, blackhearts, heartcontainers)
 			hearts_frame.bind("<Button-1>", select_build)
 			hearts_frame.grid(row=1, column=2, sticky=W)
+
 			# Removed Items
 			widget = Label(build_frame, text="Removed Items: ", bg=current_bgcolor)
 			widget.bind("<Button-1>", select_build)
@@ -307,7 +320,10 @@ def practiceWindow(root):
 		pWin.update() # Then update with the newly calculated height
 
 
-# **** Main window and installation functions ****
+# --------------------------------------
+# Main window and installation functions
+# --------------------------------------
+
 def installMod():
 	global items_xml, items_info
 	# Remove all the files and folders EXCEPT packed and tmp_folder
@@ -326,7 +342,7 @@ def installMod():
 		elif os.path.isdir(current_file):
 			shutil.copytree(current_file, os.path.join(resourcepath, resourcefile))
 
-	if practiceStart!='':
+	if practiceStart != '':
 		current_build = None
 		for build in builds:
 			if build.attrib['id'] == practiceStart:
@@ -386,7 +402,7 @@ def installMod():
 	for child in items_info:
 		if child.attrib['name']=='Black Candle':
 			child.set('hearts', redhearts)
-			child.set('maxhearts', str(int(redhearts) + int(redhearts)%2 + (int(heartcontainers) if heartcontainers else 0)))
+			child.set('maxhearts', str(int(redhearts) + (int(redhearts) % 2) + (int(heartcontainers) if heartcontainers else 0)))
 			if soulhearts:
 				child.set('soulhearts', soulhearts)
 			if blackhearts:
@@ -402,14 +418,14 @@ def installMod():
 	FNULL = open(os.devnull, 'w')
 	call(['taskkill', '/im', 'isaac-ng.exe', '/f'], stderr=FNULL)
 
-	# re/start Rebirth
+	# Start Rebirth
 	if os.path.exists(resourcepath + "/../../../../steam.exe"):
 		call([resourcepath + '/../../../../steam.exe', '-applaunch', '250900'])
 	elif os.path.exists(resourcepath + "/../isaac-ng.exe"):
 		call(resourcepath + '/../isaac-ng.exe')
 
 def uninstallMod():
-	# remove all the files and folders EXCEPT packed and tmp_folder
+	# Remove all the files and folders EXCEPT packed and tmp_folder
 	for resourcefile in os.listdir(resourcepath):
 		if resourcefile != 'packed':
 			if os.path.isfile(os.path.join(resourcepath, resourcefile)):
@@ -417,7 +433,7 @@ def uninstallMod():
 			elif os.path.isdir(os.path.join(resourcepath, resourcefile)):
 				shutil.rmtree(os.path.join(resourcepath, resourcefile))
 
-	# copy all the files and folders EXCEPT the 'packed' folder to tmp_folder
+	# Copy all the files and folders EXCEPT the 'packed' folder to tmp_folder
 	if tmp_folder:
 		for tmp_file in os.listdir(os.path.join(resourcepath, tmp_folder)):
 			if os.path.isfile(os.path.join(resourcepath, tmp_folder, tmp_file)):
@@ -425,14 +441,14 @@ def uninstallMod():
 			elif os.path.isdir(os.path.join(resourcepath, tmp_folder, tmp_file)):
 				shutil.copytree(os.path.join(resourcepath, tmp_folder, tmp_file), os.path.join(resourcepath, tmp_file))
 
-		# remove the temporary directory we created
+		# Remove the temporary directory we created
 		shutil.rmtree(os.path.join(resourcepath, tmp_folder))
 	sys.exit()
 
 def setcustompath():
-	# open file dialog
+	# Open file dialog
 	isaacpath = askopenfilename()
-	# check that the file is isaac-ng.exe and the path is good
+	# Check that the file is isaac-ng.exe and the path is good
 	if isaacpath [-12:] == "isaac-ng.exe" and os.path.exists(isaacpath[0:-12] + 'resources'):
 		customs.set('options', 'custompath', isaacpath [0:-12] + 'resources')
 		with open('options.ini', 'wb') as configfile:
@@ -461,16 +477,17 @@ if __name__ == '__main__':
 	root.tk.call('wm', 'iconphoto', root._w, get_item_icon('Libra')) # Set the GUI icon
 	root.title("Balance Mod v" + str(version)) # Set the GUI title
 
-	# check and set the paths for file creation, exit if not found
+	# Check and set the paths for file creation, exit if not found
 	currentpath = os.getcwd()
 	SteamPath = regkey_value(r"HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath")
-	# first check custom path
+	# First check the custom path from the options.ini file
 	if customs.has_option('options', 'custompath') and os.path.exists(customs.get('options', 'custompath')):
 		resourcepath = customs.get('options', 'custompath')
-	# then check steam path
+	# Second, check the steam path that we found from the registry
 	elif os.path.isdir(SteamPath + "/steamapps/common/The Binding of Isaac Rebirth/resources"):
 		resourcepath = SteamPath + "/steamapps/common/The Binding of Isaac Rebirth/resources"
-	else: # if neither, then go through the motions of writing and saving a new path to options
+	# Third, go through the motions of writing and saving a new path to options.ini
+	else:
 		feedback.set("")
 		Message(root, justify = CENTER, font = "font 10", text = "Balance Mod was unable to find your resources directory.\nNavigate to the program isaac-ng.exe in your Steam directories.", width = 600).grid(row = 0, pady = 10)
 		Message(root, justify = CENTER, font = "font 12", textvariable = feedback, width = 600).grid(row = 1)
@@ -479,7 +496,7 @@ if __name__ == '__main__':
 		mainloop()
 		sys.exit()
 
-	# Check if you're inside the resources path. give warning and close if necessary.
+	# Check if you're inside the resources path (giving warning and closing if necessary)
 	if os.path.normpath(resourcepath).lower() in os.path.normpath(currentpath).lower():
 		Message(root, justify = CENTER, font = "font 12", text = "Balance Mod is in your resources directory.\nMove it elsewhere before running.", width = 600).grid(row = 0, pady = 10)
 		mainloop()
@@ -499,7 +516,7 @@ if __name__ == '__main__':
 		install_check = "Balance Mod"
 	if install_check[:11] != 'Balance Mod':
 		seed()
-		tmp_folder = '../resources_tmp' + str(randint(1000000000,9999999999))
+		tmp_folder = '../resources_tmp' + str(randint(100000, 999999))
 		if not os.path.exists(os.path.join(resourcepath, tmp_folder)):
 			os.mkdir(os.path.join(resourcepath, tmp_folder))
 
@@ -512,7 +529,7 @@ if __name__ == '__main__':
 					elif os.path.isdir(os.path.join(resourcepath, resourcefile)):
 						shutil.copytree(os.path.join(resourcepath, resourcefile), os.path.join(resourcepath, tmp_folder, resourcefile))
 				except Exception, e:
-					print e
+					print 'Exception occured when copying the files in the Rebirth resources folder to a temporary location: ' + e
 	else:
 		tmp_folder = None
 
